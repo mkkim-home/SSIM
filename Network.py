fe2f2a6d29c1ed4391042a3988497fdbd7e755ed
minkyung-kim17 <mkkim1778@gmail.com>
2019-08-02 17:56:12 +0900

    draft:version-cpu

--- Network.py
+++ Network.py
@@ -17,14 +17,14 @@ class SSIM(nn.Module):
         self.dec_hid_size = dec_hid_size  # size of hidden state at Decoder
         self.output_size = output_size
 
-        self.enc_layer = 1
-        self.dec_layer = 1
+        self.enc_layer = 2
+        self.dec_layer = 2
 
         self.dropout = 0.2
 
         self.encoder = nn.LSTM(self.input_size, self.enc_hid_size,
                                num_layers=self.enc_layer, batch_first=True,
-                               dropout=self.dropout, bidirectional=True)  # 여기 hidden_size size확인
+                               dropout=self.dropout, bidirectional=True)
         self.attn = nn.Linear(self.dec_hid_size + self.enc_hid_size * 2,
                                    self.dec_hid_size)
         self.v = nn.Parameter(torch.rand(self.dec_hid_size))
@@ -36,105 +36,108 @@ class SSIM(nn.Module):
 
     def forward(self, input):
         if isinstance(input, torch.nn.utils.rnn.PackedSequence):
-            # batch_size = input.data.size(0)
-            # src_len = input.data.size(1)
-            pass
+            ''' encoder '''
+            packed_output, (enc_h, enc_c) = self.encoder(input)
+            encoder_outputs, input_lengths = pad_packed_sequence(packed_output, batch_first=True)
+
+            batch_size = encoder_outputs.size(0)
+            src_len = encoder_outputs.size(1)
+
+            output = torch.zeros(batch_size, self.output_size)
+
+            ''' decoder with attention '''
+            dec_h0 = torch.randn(self.dec_layer, batch_size, self.dec_hid_size)
+            dec_c0 = torch.randn(self.dec_layer, batch_size, self.dec_hid_size)
+
+            y0 = torch.zeros(self.output_size).repeat(batch_size)  # ?????
+
+            hidden = dec_h0
+
+            y = y0.unsqueeze(1)
+            for i in range(src_len):
+                ''' attention '''
+                hidden = torch.unbind(hidden, dim=0)[0]  # 마지막 layer가 [0]이 맞나?
+                hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
+
+                attentions = torch.zeros(1, src_len)
+                for batch_idx, input_length in enumerate(input_lengths):
+                    energy = torch.tanh(self.attn(torch.cat((hidden[batch_idx, :input_length, :], encoder_outputs[batch_idx, :input_length, :]), dim=1)))
+                    energy = energy.permute(1, 0)
+
+                    attention = self.v.unsqueeze(0).mm(energy)
+                    attention = F.softmax(attention, dim=1)
+                    attention = torch.cat((attention, torch.zeros(src_len - input_length).unsqueeze(0)), dim=1)
+
+                    attentions = torch.cat((attentions, attention), dim=0)
+                attention = attentions[1:, :].unsqueeze(2).repeat(1, 1, self.enc_hid_size * 2)
+
+                ''' context '''
+                context = attention * encoder_outputs
+                context = torch.sum(context, dim=1)  # 각 input의 유효 sequence 길이 만큼의 h_i만 weighted sum
+
+                ''' decoder lstm '''
+                dec_input = torch.cat((y, context), dim=1)
+
+                decoder_outputs, (dec_h, dec_c) = self.decoder(dec_input.unsqueeze(1), (dec_h0, dec_c0))
+
+                ''' dense layer '''
+                lin_input = torch.cat((decoder_outputs.squeeze(), context), dim=1)
+
+                y = self.linear(lin_input)
+                output = torch.cat((output, y), dim=1)
+
+                hidden = decoder_outputs.permute(1, 0, 2)
+
         else:
             batch_size = input.size(0)
             src_len = input.size(1)
 
-        # output = torch.zeros(batch_size, self.output_size)
+            output = torch.zeros(batch_size, self.output_size)
 
-        # pdb.set_trace() ##################################################################################
+            ''' encoder '''
+            enc_h0 = torch.randn(self.enc_layer * 2, batch_size, self.enc_hid_size)  # 2 for bi-LSTM
+            enc_c0 = torch.randn(self.enc_layer * 2, batch_size, self.enc_hid_size)
 
-        ''' encoder '''
-        # enc_h0 = torch.randn(self.enc_layer * 2, batch_size, self.enc_hid_size)  # 2 for bi-LSTM
-        # enc_c0 = torch.randn(self.enc_layer * 2, batch_size, self.enc_hid_size)
-        # encoder_outputs, (enc_h, enc_c) = self.encoder(input, (enc_h0, enc_c0))
+            encoder_outputs, (enc_h, enc_c) = self.encoder(input, (enc_h0, enc_c0))
 
-        packed_output, (enc_h, enc_c) = self.encoder(input)
-        encoder_outputs, input_lengths = pad_packed_sequence(packed_output, batch_first=True)
-        # print(input_lengths)
+            ''' decoder with attention '''
+            dec_h0 = torch.randn(self.dec_layer, batch_size, self.dec_hid_size)
+            dec_c0 = torch.randn(self.dec_layer, batch_size, self.dec_hid_size)
 
-        # pdb.set_trace()
+            y0 = torch.zeros(self.output_size).repeat(batch_size)  # ?????
 
-        if isinstance(input, torch.nn.utils.rnn.PackedSequence):
-            batch_size = encoder_outputs.size(0)
-            src_len = encoder_outputs.size(1)
+            hidden = dec_h0
+            y = y0.unsqueeze(1)  # output dimension이 달라지면, unsqueeze 확인!
+            for i in range(src_len):
+                ''' attention '''
+                hidden = torch.unbind(hidden, dim=0)[0]
+                hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
 
-        output = torch.zeros(batch_size, self.output_size)
+                energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
+                energy = energy.permute(0, 2, 1)
 
-        ''' decoder with attention '''
-        dec_h0 = torch.randn(self.dec_layer, batch_size, self.dec_hid_size)
-        dec_c0 = torch.randn(self.dec_layer, batch_size, self.dec_hid_size)
+                v = self.v.repeat(batch_size, 1).unsqueeze(1)
 
-        y0 = torch.zeros(self.output_size).repeat(batch_size)  # decoder with attention을 어떻게 시작해야 하는지 모르겠다...
-        # pdb.set_trace()
-
-        hidden = dec_h0
-        y = y0.unsqueeze(1)  # output dimension이 달라지면, 여기 unsqueeze 달라져야 할지도...
-        for i in range(src_len):
-            ''' attention '''
-            hidden = torch.unbind(hidden, dim=0)[0]
-            hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
-
-            # pdb.set_trace()
-
-            attentions = torch.zeros(1, src_len)
-            for batch_idx, input_length in enumerate(input_lengths):
-                energy = torch.tanh(self.attn(torch.cat((hidden[batch_idx, :input_length, :], encoder_outputs[batch_idx, :input_length, :]), dim=1)))
-                energy = energy.permute(1, 0)
-
-                attention = self.v.unsqueeze(0).mm(energy)
+                attention = torch.bmm(v, energy).squeeze(1)
                 attention = F.softmax(attention, dim=1)
-                attention = torch.cat((attention, torch.zeros(src_len - input_length).unsqueeze(0)), dim=1)
 
-                attentions = torch.cat((attentions, attention), dim=0)
+                attention = attention.unsqueeze(2).repeat(1, 1, self.enc_hid_size * 2)
 
-            # energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
-            # energy = energy.permute(0, 2, 1)
+                ''' context '''
+                context = attention * encoder_outputs
+                context = torch.sum(context, dim=1)  # 각 input의 유효 sequence 길이 만큼의 h_i만 weighted sum
 
-            # energy 만든거 중에서, pad 된 부분은 0으로 처리 <- inplace operation이라서 backward가 안됨... ㅜ.ㅜ
-            # for input_idx, input_length in enumerate(input_lengths):
-                # pdb.set_trace()
-                # energy[input_idx][:, input_length:] = -(np.inf) * energy[input_idx][:, input_length:].clone()
-                # for no effect when calculating softmax
+                ''' decoder lstm '''
+                dec_input = torch.cat((y, context), dim=1)
 
-            # pdb.set_trace()
+                decoder_outputs, (dec_h, dec_c) = self.decoder(dec_input.unsqueeze(1), (dec_h0, dec_c0))
 
-            # v = self.v.repeat(batch_size, 1).unsqueeze(1)
+                ''' dense layer '''
+                lin_input = torch.cat((decoder_outputs.squeeze(), context), dim=1)
 
-            # attention = torch.bmm(v, energy).squeeze(1)
-            # attention = F.softmax(attention, dim=1)  # [batch_size, src_len]
+                y = self.linear(lin_input)
+                output = torch.cat((output, y), dim=1)
 
-            # attention = attention.unsqueeze(2).repeat(1, 1, self.enc_hid_size * 2)
-
-            # pdb.set_trace()
-            attention = attentions[1:, :].unsqueeze(2).repeat(1, 1, self.enc_hid_size * 2)
-
-            # pdb.set_trace()
-
-            ''' context '''
-            context = attention * encoder_outputs
-            context = torch.sum(context, dim=1)  # 각 input의 유효 sequence 길이 만큼의 h_i만 weighted sum
-
-            ''' decoder lstm '''
-            dec_input = torch.cat((y, context), dim=1)
-
-            decoder_outputs, (dec_h, dec_c) = self.decoder(dec_input.unsqueeze(1), (dec_h0, dec_c0))
-
-            ''' dense layer '''
-            lin_input = torch.cat((decoder_outputs.squeeze(), context), dim=1)
-
-            # pdb.set_trace()
-            y = self.linear(lin_input)
-            output = torch.cat((output, y), dim=1)
-
-            hidden = decoder_outputs.permute(1, 0, 2)
-
-        # pdb.set_trace()
-
-        # if output.size(1) != 20:
-            # pdb.set_trace()
+                hidden = decoder_outputs.permute(1, 0, 2)
 
         return output[:, 1:]
--- dataUtil.py
+++ dataUtil.py
@@ -11,20 +11,49 @@ class Data:
         self.raw_data_path = os.path.join(self.data_path, 'raw')
 
         ''' VLSW parameter '''
-        self.m = 2  # minimum length of the left input sequence
-        self.n = 7  # maximum length of the left input sequence
-        self.o = 2  # minimum length of the right input sequence
-        self.p = 7  # maximum length of the right input sequence
-        self.q = 5  # missing legnth
+        # self.m = 2  # minimum length of the left input sequence
+        # self.n = 7  # maximum length of the left input sequence
+        # self.o = 2  # minimum length of the right input sequence
+        # self.p = 7  # maximum length of the right input sequence
+        # self.q = 5  # missing legnth
 
     def load_data(self, filename):
         return torch.load(os.path.join(self.data_path, filename))
 
-    def VLSW(self, data, mulvar=True, data_idx=0):
-        if not mulvar:  # mulvar은 나중에 생각해보자!
+    def preprocessing(self, input, target, lengths):
+        print('DATA PREPROCESSING')
+
+        print('\t1. NaN data check (deletion method)')
+        nan_r_idx = np.unique(np.concatenate((np.where(np.isnan(input) == True)[0],
+                                    np.where(np.isnan(target) == True)[0])))
+        nan_r_idx[::-1].sort()  # for delete rows
+
+        if len(nan_r_idx) > 0:
+            print('\t\tWarning!!')
+            print('\t\t{} rows with NaN data detected in train data'.format(nan_r_idx.shape[0]))
+
+        for r_idx in nan_r_idx:
+            input = np.delete(input, r_idx, axis=0)  # delete row
+            target = np.delete(target, r_idx, axis=0)  # delete row
+            lengths = np.delete(lengths, r_idx)
+        print('\t\tNb. of train data: {} (NaN data are deleted)'.format(input.shape[0]))
+
+        print('\t2. Convert data into torch.Tensor')
+        input = torch.from_numpy(input)
+        target = torch.from_numpy(target)
+        lengths = torch.from_numpy(lengths)
+
+        print('\t3. Convert data into float (optional)')
+        input = input.float()
+        target = target.float()
+
+        return input, target, lengths
+
+    def VLSW(self, data, m, n, o, p, q, mulvar=True, data_idx=0):
+        if not mulvar:
             data = data[data_idx, :]
 
-        # packed_sequence 고려.
+        # draft of this function
         # train_input = []
         # train_output = []
         # print('Left {} || Right {} || length {}'.format(0, 0, 0))
@@ -37,33 +66,33 @@ class Data:
         #                                np.zeros(self.q),
         #                                data[idx+len_left+self.q:idx+len_input])))
         #             train_output.append(data[idx:idx+len_input])
-        #             # train_output.append(data[idx+len_left:idx+len_left+self.q])
-        #             # 이렇게 적혀있긴 한데, train data 마다마다 제각기 다른 길이를, model output에서 잘라낼 방법을 모르겠음...
-        #             # 어차피 encoder-decoder 구조니까, 그냥 input == output 구조로 가자
-
+        #             # train_output.append(data[idx+len_left:idx+len_left+self.q])  # only data at missing period
+        #             # 논문에는 output의 size가 missing length와 동일하다고 되어있는데,
+        #             # missing length 길이만큼만 decoding을 할때, 값의 시작을 무엇으로 해야할지 모르겠음...
+        #             # 그래서, 어차피 encoder-decoder 구조니까, input == output으로 가보자
         # return np.array(train_input), np.array(train_output)
 
         nb_train_data = 0
-        for len_left in range(self.m, self.n+1):
-            for len_right in range(self.o, self.p+1):
-                len_input = len_left + len_right + self.q
+        for len_left in range(m, n+1):
+            for len_right in range(o, p+1):
+                len_input = len_left + len_right + q
                 nb_train_data += (len(data) - len_input + 1)
-        maximum_data_length = self.n + self.q + self.p
+        maximum_data_length = n + q + p
 
         train_input = np.zeros((nb_train_data, maximum_data_length))
         train_output = np.zeros((nb_train_data, maximum_data_length))
         train_lengths = np.zeros((nb_train_data))
 
         i = 0
-        for len_left in range(self.m, self.n+1):
-            for len_right in range(self.o, self.p+1):
-                len_input = len_left + len_right + self.q
+        for len_left in range(m, n+1):
+            for len_right in range(o, p+1):
+                len_input = len_left + len_right + q
                 print('Left {} || Right {} || length {}'.format(len_left, len_right, len_input))
                 for idx in range(len(data) - len_input + 1):
                     train_input[i, :len_input] = np.concatenate((data[idx:idx+len_left],
-                                                                 np.zeros(self.q),
-                                                                 data[idx+len_left+self.q:idx+len_input]))
-                    train_output[i, :len_input] = data[idx:idx+len_input]  # 일단 임의로 내가 input = output
+                                                                 np.zeros(q),
+                                                                 data[idx+len_left+q:idx+len_input]))
+                    train_output[i, :len_input] = data[idx:idx+len_input]  # ?????
                     train_lengths[i] = len_input
                     i += 1
         sorted_idx = np.argsort(train_lengths)[::-1]  # descending order
@@ -74,12 +103,11 @@ class Data:
 
         return train_input, train_output, train_lengths
 
+
     def _rs_inteldata(self, filename, filename_save):  # read and save
         data_folder = 'intellabdata'
         data_path = os.path.join(self.raw_data_path, data_folder)
 
-        # pdb.set_trace()
-
         if filename == 'inteldata_44.npy':
             data = np.load(os.path.join(data_path, filename))  # (21, 48, 3)
 
@@ -95,22 +123,18 @@ class Data:
             test_data = np.array(test_data)
 
             ''' VLSW '''
-            train_input, train_output = self.VLSW(train_data, mulvar=False, data_idx=0)
+            train_input, train_output = self.VLSW(train_data, m=2, n=7, o=2, p=7, q=5, mulvar=False, data_idx=0)
             # IL + M + IR / O
 
             # save numpy as .pt datafile
             torch.save(train_input, open(os.path.join(self.data_path, filename_save[0]), 'wb'))
             torch.save(train_output, open(os.path.join(self.data_path, filename_save[1]), 'wb'))
 
-            ## test_data도 VLSW로 만들어둬야 할거 같은데...
-            ## 우선 train이랑 validation만 해보자!
 
-    def _rs_inteldata_packed(self, filename, filename_save):  # read and save
+    def _rs_inteldata_packed(self, filename, tr_filename_save, te_filename_save):  # read and save
         data_folder = 'intellabdata'
         data_path = os.path.join(self.raw_data_path, data_folder)
 
-        # pdb.set_trace()
-
         if filename == 'inteldata_44.npy':
             data = np.load(os.path.join(data_path, filename))  # (21, 48, 3)
 
@@ -126,13 +150,31 @@ class Data:
             test_data = np.array(test_data)
 
             ''' VLSW '''
-            train_input, train_output, train_lengths = self.VLSW(train_data, mulvar=False, data_idx=0)
+            m = 2
+            n = 7
+            o = 2
+            p = 7
+            q = 5
+            train_input, train_output, train_lengths = self.VLSW(train_data, m, n, o, p, q, mulvar=False, data_idx=0)
             # IL + M + IR / O
 
             # save numpy as .pt datafile
-            torch.save(train_input, open(os.path.join(self.data_path, filename_save[0]), 'wb'))
-            torch.save(train_output, open(os.path.join(self.data_path, filename_save[1]), 'wb'))
-            torch.save(train_lengths, open(os.path.join(self.data_path, filename_save[2]), 'wb'))
+            torch.save(train_input, open(os.path.join(self.data_path, tr_filename_save[0]), 'wb'))
+            torch.save(train_output, open(os.path.join(self.data_path, tr_filename_save[1]), 'wb'))
+            torch.save(train_lengths, open(os.path.join(self.data_path, tr_filename_save[2]), 'wb'))
+
+
+            # for test dataset,
+            # make test dataset for every combination of length of left side and right side.
+            for len_left in range(m, n+1):
+                for len_right in range(o, p+1):
+                    test_input, test_output, test_lengths = self.VLSW(test_data,
+                    m=len_left, n=len_left, o=len_right, p=len_right, q=q, mulvar=False, data_idx=0)
+
+                    torch.save(test_input, open(os.path.join(self.data_path, (te_filename_save[0] + '_{}_{}.pt'.format(len_left, len_right))), 'wb'))
+                    torch.save(test_output, open(os.path.join(self.data_path, (te_filename_save[1] + '_{}_{}.pt'.format(len_left, len_right))), 'wb'))
+                    torch.save(test_lengths, open(os.path.join(self.data_path, (te_filename_save[2] + '_{}_{}.pt'.format(len_left, len_right))), 'wb'))
+
 
 if __name__ == '__main__':
     current_path = inspect.getfile(inspect.currentframe())  # 'Utils.py'
@@ -149,6 +191,10 @@ if __name__ == '__main__':
     data = dataUtil._rs_inteldata_packed('inteldata_44.npy',
                             ['ssim_tr_in_pack_intel44.pt',
                              'ssim_tr_out_pack_intel44.pt',
-                             'ssim_tr_len_pack_intel44.pt'])
+                             'ssim_tr_len_pack_intel44.pt'],
+                            ['ssim_te_in_pack_intel44',
+                             'ssim_te_out_pack_intel44',
+                             'ssim_te_len_pack_intel44'])
+
 
     # pdb.set_trace()
